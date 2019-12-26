38dd64544ac9d4ad6d31b6cbd819833436478424
manasfonia <manasfonia19@gmail.com>
2019-12-26 10:38:43 +0530

    Second commit

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -4,6 +4,9 @@ from django.contrib.auth.views import LoginView
 
 urlpatterns = [
             path('', LoginView.as_view(), name='login'),
-            path('base/', views.DisplayView, name='base')
-
+            path('base/', views.DisplayView, name='base'),
+            path('accounts/profile/fix/', views.FixView, name='fix'),
+            path('accounts/profile/', views.ProfileView, name='profile'),
+            path('accounts/profile/employdata', views.EmView, name='emdata'),
+            path('accounts/profile/add', views.AddEmployer, name='add_employer')
        ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,7 +1,27 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.template import context
 
+from .models import EmployeeInfo, VisitorInfo
 
 # Create your views here.
 def DisplayView(request):
     return render(request, "base.html")
+
+def FixView(request):
+    return render(request, "fixmeet.html")
+
+def ProfileView(request):
+    return render(request, "profile.html")
+
+def EmView(request):
+    model = EmployeeInfo
+    context ={
+        'designation': model.designation,
+        'user': model.user,
+        'phone': model.phone
+    }
+    return render(request, "employeedata.html")
+
+def AddEmployer(request):
+    return render(request, "addempolyer.html")
